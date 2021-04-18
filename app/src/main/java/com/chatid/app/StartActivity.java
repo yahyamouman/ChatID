825c4a43ef4a729ffe1deac299e372c7b0b714a7
yahyamouman <yahyamouman@gmail.com>
2021-04-18 16:50:26 +0000

    added auto login to StartActivity.java

--- app/src/main/java/com/chatid/app/StartActivity.java
+++ app/src/main/java/com/chatid/app/StartActivity.java
@@ -7,14 +7,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class StartActivity extends AppCompatActivity {
 
     Button login, register;
+
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (firebaseUser != null){
+            startActivity(new Intent(StartActivity.this, MainActivity.class));
+        }
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
