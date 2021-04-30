7b52554294b40d801c18dc9c1f2ccd9ff2840387
yahyamouman <yahyamouman@gmail.com>
2021-04-30 02:20:08 +0000

    added MessageActivity (part 6)

--- app/src/main/java/com/chatid/app/Adapter/UserAdapter.java
+++ app/src/main/java/com/chatid/app/Adapter/UserAdapter.java
@@ -1,6 +1,7 @@
 package com.chatid.app.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.chatid.app.MessageActivity;
 import com.chatid.app.Model.User;
 import com.chatid.app.R;
 
@@ -41,6 +43,15 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         } else {
             Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
         }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MessageActivity.class);
+                intent.putExtra("userid", user.getId());
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     @Override
